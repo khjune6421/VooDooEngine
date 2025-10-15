b8cae907b365ab1ed0ff97df5c771c9b05ba6437
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-10-15 14:09:33 +0900

    Update VertexShader.hlsl

--- Assets/Default/Shader/VertexShader/DefaultInputLayout/VertexShader.hlsl
+++ Assets/Default/Shader/VertexShader/DefaultInputLayout/VertexShader.hlsl
@@ -49,10 +49,8 @@ VSOutput main(VSInput input)
     float3 normal = normalize(input.norm0);
     
     float diffuseFactor;
-    if (isBackfaceLighting)
-        diffuseFactor = abs(dot(normal, lightDir));
-    else
-        diffuseFactor = max(dot(normal, lightDir), 0.0f);
+    if (isBackfaceLighting) diffuseFactor = abs(dot(normal, lightDir));
+    else diffuseFactor = max(dot(normal, lightDir), 0.0f);
     
     float attenuationFactor = 1.0f;
     if (distance > 0.0f) attenuationFactor = 1.0f / (1.0f + attenuation * distance * distance); // + 1.0f to prevent div by 0 // TODO: change this
