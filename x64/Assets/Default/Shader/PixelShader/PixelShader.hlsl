795fce792702608c823815d2df06237dcb74577c
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-10-16 13:23:33 +0900

    asdasd

--- x64/Assets/Default/Shader/PixelShader/PixelShader.hlsl
+++ x64/Assets/Default/Shader/PixelShader/PixelShader.hlsl
@@ -1,12 +1,18 @@
+Texture2D _MainTex;
+SamplerState sampler_MainTex;
+
 struct PSInput
 {
     float4 pos : SV_POSITION0;
     float4 col : COLOR0;
-    
     float4 light : TEXCOORD0;
+    float2 uv : TEXCOORD1;
 };
 
 float4 main(PSInput input) : SV_TARGET
 {
-    return input.col * (input.light);
+    float4 diffuse = input.col * input.light;
+    float4 texColor = _MainTex.Sample(sampler_MainTex, input.uv);
+    
+    return texColor * diffuse;
 }
