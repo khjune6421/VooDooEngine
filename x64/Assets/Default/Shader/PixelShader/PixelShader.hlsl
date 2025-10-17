d6dc3563983d3ac893b616ca761a6d15cff7d42f
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-10-17 14:33:19 +0900

    adasd

--- x64/Assets/Default/Shader/PixelShader/PixelShader.hlsl
+++ x64/Assets/Default/Shader/PixelShader/PixelShader.hlsl
@@ -6,18 +6,46 @@ cbuffer AmbientLightConstBuffer : register(b0)
     float4 ambientColor;
 }
 
+struct PointLight
+{
+    float4 worldPos;
+    float4 color; // w is intensity
+    float range;
+    
+    // Attenuation factors
+    float aConstant;
+    float aLinear;
+    float aQuadratic;
+};
+
+cbuffer PointLightConstBuffer : register(b1)
+{
+    PointLight pointLights;
+}
+
 struct PSInput
 {
     float4 pos : SV_POSITION0;
     float4 col : COLOR0;
     float3 norm : NORMAL0;
     float2 uv : TEXCOORD0;
+    
+    float4 posWorld : TEXCOORD1;
 };
 
 float4 main(PSInput input) : SV_TARGET
 {
-    float4 diffuse = input.col * input.light;
     float4 texColor = _MainTex.Sample(sampler_MainTex, input.uv);
     
-    return texColor * diffuse;
+    float3 vecToLight = pointLights.worldPos.xyz - input.posWorld.xyz;
+    float3 norm = normalize(input.norm);
+    float diffuseFactor = saturate(dot(norm, normalize(vecToLight)));
+    
+    float distance = length(vecToLight);
+    float3 attenuate_constants = float3(pointLights.aConstant, pointLights.aLinear, pointLights.aQuadratic);
+    float attenuate = 1.0f / dot(attenuate_constants, float3(1.0f, distance, distance * distance));
+    
+    float4 diffuseColor = pointLights.color * diffuseFactor * attenuate * pointLights.color.w;
+    
+    return texColor * (diffuseColor + ambientColor);
 }
