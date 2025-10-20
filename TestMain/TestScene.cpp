f80a174bd66ad6d278e6de42a4b0b16fdb7318b9
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-10-20 12:36:02 +0900

    Update TestScene.cpp

--- TestMain/TestScene.cpp
+++ TestMain/TestScene.cpp
@@ -10,6 +10,8 @@ TestScene::TestScene(wstring dataFile)
 	m_camera->AddComponent<Camera>();
 
 	m_player->AddChild(m_camera.get());
+	m_player->SetPosition(XMVECTOR{ 0.0f, 1.0f, 0.0f, 1.0f });
+	m_player->AddComponent<PointLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 3.0f);
 	m_windmill->SetPosition(XMVECTOR{ 5.0f, 0.0f, 5.0f, 1.0f });
 	m_windmill->SetScale(XMFLOAT3{ 1.5f, 1.5f, 1.5f });
 
@@ -20,9 +22,8 @@ TestScene::TestScene(wstring dataFile)
 
 	unique_ptr<Object> lightObj = make_unique<Object>();
 	lightObj->SetPosition(XMVECTOR{ 5.0f, 5.0f, 0.0f, 1.0f });
-	lightObj->AddComponent<AmbientLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 0.25f);
-	lightObj->AddComponent<DirectionalLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 0.5f, XMVECTOR{ -1.0f, -1.0f, 1.0f, 0.0f });
-	lightObj->AddComponent<PointLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 5.0f);
+	//lightObj->AddComponent<AmbientLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 0.1f);
+	//lightObj->AddComponent<DirectionalLight>(XMFLOAT3{ 1.0f, 1.0f, 1.0f }, 0.1f, XMVECTOR{ -1.0f, -1.0f, 1.0f, 0.0f });
 	lightObj->AddComponent<Shape>(L"Sphere");
 	m_objects.emplace_back(move(lightObj));
 
