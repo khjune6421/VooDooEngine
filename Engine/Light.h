8f248cf7bfac3e60560b9cbc7510c9a89143645c
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-10-31 16:23:43 +0900

    asdasd

--- Engine/Light.h
+++ Engine/Light.h
@@ -1,9 +1,7 @@
 #pragma once
-
 #include "pch.h"
 
 #include "Component.h"
-
 #include "Object.h"
 
 class Light : public Component
--- Engine/Renderer.h
+++ Engine/Renderer.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "pch.h"
 
 // Other header files
