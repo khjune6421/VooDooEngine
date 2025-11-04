2da82e72ab16e10dda8cd031965e03d795e075ac
khjune6421 <98323466+khjune6421@users.noreply.github.com>
2025-11-04 11:02:47 +0900

    Update TestScene.cpp

--- TestMain/TestScene.cpp
+++ TestMain/TestScene.cpp
@@ -9,9 +9,9 @@ TestScene::TestScene()
 	m_ambientFog.x = m_timeColors[m_timeOfDay].x;
 	m_ambientFog.y = m_timeColors[m_timeOfDay].y;
 	m_ambientFog.z = m_timeColors[m_timeOfDay].z;
-	m_ambientLight = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.25f, m_timeColors[m_timeOfDay].y * 0.25f, m_timeColors[m_timeOfDay].z * 0.25f, 1.0f };
+	m_ambientLight = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.5f, m_timeColors[m_timeOfDay].y * 0.5f, m_timeColors[m_timeOfDay].z * 0.5f, 1.0f };
 
-	m_directionalLight.color = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.75f, m_timeColors[m_timeOfDay].y * 0.75f, m_timeColors[m_timeOfDay].z * 0.75f, 0.0f };
+	m_directionalLight.color = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.5f, m_timeColors[m_timeOfDay].y * 0.5f, m_timeColors[m_timeOfDay].z * 0.5f, 0.0f };
 	m_directionalLight.direction = XMVECTOR{ 1.0f, -1.0f, 1.0f, 0.0f };
 
 	m_camera->SetPosition(XMVECTOR{ 0.0f, 10.0f, -20.0f, 1.0f });
@@ -69,8 +69,8 @@ void TestScene::Update(float deltaTime)
 		m_ambientFog.x = m_timeColors[m_timeOfDay].x;
 		m_ambientFog.y = m_timeColors[m_timeOfDay].y;
 		m_ambientFog.z = m_timeColors[m_timeOfDay].z;
-		m_ambientLight = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.25f, m_timeColors[m_timeOfDay].y * 0.25f, m_timeColors[m_timeOfDay].z * 0.25f, 1.0f };
-		m_directionalLight.color = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.75f, m_timeColors[m_timeOfDay].y * 0.75f, m_timeColors[m_timeOfDay].z * 0.75f, 0.0f };
+		m_ambientLight = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.5f, m_timeColors[m_timeOfDay].y * 0.5f, m_timeColors[m_timeOfDay].z * 0.5f, 1.0f };
+		m_directionalLight.color = XMFLOAT4{ m_timeColors[m_timeOfDay].x * 0.5f, m_timeColors[m_timeOfDay].y * 0.5f, m_timeColors[m_timeOfDay].z * 0.5f, 0.0f };
 	}
 
 	if (GetAsyncKeyState('W') & 0x8000) m_torch->Rotate(XMVECTOR{ -1.0f * deltaTime, 0.0f, 0.0f, 0.0f });
